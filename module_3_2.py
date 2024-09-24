e7ce42d37e0946afef19ab8e08ae81422f16e6f5
arudneva <annarudneva10@gmail.com>
2024-09-24 19:49:26 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,10 +1,10 @@
-def send_email(message, recipient, sender="university.help@gmail.com"):
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
     if "@" not in recipient or "@" not in sender or not (recipient.endswith(".com") or recipient.endswith(".ru") or recipient.endswith(".net") or sender.endswith(".com") or sender.endswith(".ru") or sender.endswith(".net")):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
         return
     if sender == recipient:
         print('Нельзя отправить письмо самому себе!')
-        return 
+        return
     if sender == "university.help@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
     else:
