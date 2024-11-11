8dec187b2ece4d9cc6dd926fc822b49a0454d3b5
arudneva <annarudneva10@gmail.com>
2024-11-11 23:15:06 +0300

    Add files via upload

--- module_8_2.py
+++ module_8_2.py
@@ -6,7 +6,7 @@ def personal_sum(numbers):
             result += number
         except TypeError:
             incorrect_data += 1
-            print(f'Некорректный тип данных для подсчёта суммы - ')
+            print(f'Некорректный тип данных для подсчёта суммы - {number}')
 
 
     return result, incorrect_data
